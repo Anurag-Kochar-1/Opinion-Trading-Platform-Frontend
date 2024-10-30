922b03c01707f1bac4cf04e2d2be502694dd550b
anurag-kochar <anuragmarketing101@gmail.com>
2024-10-31 02:25:23 +0530

    🔨 refactor: hard-coded backend url stored into env

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,4 +1,4 @@
 import axios from "axios"
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:3000/',
 });
