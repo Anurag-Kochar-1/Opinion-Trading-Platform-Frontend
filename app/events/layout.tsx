4e5dada5affb9377c49030cc389384f6f78d3db7
anurag-kochar <anuragmarketing101@gmail.com>
2024-11-02 03:59:16 +0530

    ⚒  add: failure reason in /events layout for ping queries

--- app/events/layout.tsx
+++ app/events/layout.tsx
@@ -5,10 +5,24 @@ import {
   usePingEngineServerQuery,
 } from "@/services/health/queries";
 import Image from "next/image";
+import { useMemo } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { isError: isEngineError } = usePingEngineServerQuery();
   const { isError: isApiBackendError } = usePingApiBackendServerQuery();
+
+  const failureReason: string = useMemo(() => {
+    if (isApiBackendError && !isEngineError) {
+      return "Server";
+    }
+    if (isEngineError && !isApiBackendError) {
+      return "Trading Engine";
+    }
+    if (isApiBackendError && isEngineError) {
+      return "Trading Engine and Server";
+    }
+    return "Trading Engine or Server";
+  }, [isApiBackendError, isEngineError]);
 
   if (isEngineError || isApiBackendError)
     return (
@@ -18,14 +32,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             "https://media1.tenor.com/m/2gyy4BcsLWsAAAAd/monkey-confused.gif"
           }
           alt="sad-monke"
-          width={ 250}
+          width={250}
           height={250}
           quality={50}
           className="max-w-sm"
         />
         <p className="font-semibold text-lg md:text-2xl max-w-sm text-center">
-          Our Trading Engine and Server is down. Please try again in a few
-          hours.
+          Our {failureReason} is down. Please try again in a few hours.
         </p>
       </div>
     );
